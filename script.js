09250d6d83d95bd436e882d6944dcf55751cdd25
Carlos Herver <carlos.h.s.1994@gmail.com>
2021-12-23 13:34:17 -0600

    Add feature to generate the courses dinamicaly

--- script.js
+++ script.js
@@ -10,3 +10,48 @@ closeMenu.addEventListener('click', openAndClose);
 closeNavLink.forEach((element) => {
   element.addEventListener('click', openAndClose);
 });
+//-------------------------------------------Array of Courses
+const arrOfFavoriteCourses = [
+  {
+    title: 'Algebra 1',
+    content: 'Foundation, Solving equations, working with units, linear equations & graphs, Forms of linear equations, System of equations',
+    linkToCompleteCourse: '#',
+  },
+  {
+    title: 'Organic Chemistry',
+    content: '',
+    linkToCompleteCourse: '#',
+  },
+  {
+    title: 'Trigonometry',
+    content: '',
+    linkToCompleteCourse: '#',
+  },
+  {
+    title: 'Probability',
+    content: '',
+    linkToCompleteCourse: '#',
+  },
+  {
+    title: 'Precalculus',
+    content: '',
+    linkToCompleteCourse: '#',
+  },
+  {
+    title: "Newton's laws",
+    content: '',
+    linkToCompleteCourse: '#',
+  }
+];
+
+const favoriteCourse = arrOfFavoriteCourses.map((element, index) => {
+  const course = document.createElement('article');
+  course.className = 'card';
+  course.innerHTML = `<div class="background-card"><h2>${element.title}</h2></div>`;
+  return course;
+});
+console.log(favoriteCourse[0])
+const createFavoriteCourses = document.getElementById('courses-wrap');
+for (let i = 0; i<favoriteCourse.length; i += 1){
+  createFavoriteCourses.appendChild(favoriteCourse[i]);
+}
