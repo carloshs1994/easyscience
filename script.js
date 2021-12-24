b2dbd2e7ee9aab0212fe8106827871984f03b224
Carlos Herver <carlos.h.s.1994@gmail.com>
2021-12-24 12:58:48 -0600

    Fix the courses section inside index.html file

--- script.js
+++ script.js
@@ -16,31 +16,37 @@ const arrOfFavoriteCourses = [
     title: 'Algebra 1',
     content: 'Content: Foundation, Solving equations, working with units, linear equations & graphs, Forms of linear equations, System of equations',
     linkToCompleteCourse: '#',
+    image: 'assets/Roulette70.svg',
   },
   {
     title: 'Organic Chemistry',
     content: 'Content: Structure and bonding, Resonance and acid-base chemistry, Alkanes, cycloalkanes, and functional groups, Stereochemistry, Substitution and elimination reactions, Alkenes and alkynes, Alcohols, ethers, epoxides, sulfides, Conjugated systems and pericyclic reactions, Aromatic compounds, Aldehydes and ketones, Carboxylic acids and derivatives, Alpha carbon chemistry, Amines, Spectroscopy',
     linkToCompleteCourse: '#',
+    image: 'assets/toluene.svg',
   },
   {
     title: 'Trigonometry',
     content: 'Content: Right triangles & trigonometry, Trigonometric functions, Non-right triangles & trigonometry, Trigonometric equations and identities',
     linkToCompleteCourse: '#',
+    image: 'assets/Roulette70.svg',
   },
   {
     title: 'Probability',
     content: 'Content: Simple probability, Counting permutations and combinations, Random variables, Sampling distributions, Confidence intervals, Significance tests (hypothesis testing)',
     linkToCompleteCourse: '#',
+    image: 'assets/Roulette70.svg',
   },
   {
     title: 'Precalculus',
     content: 'Content: Composite and inverse functions, Trigonometry, Complex numbers, Rational functions, Conic sections, Vectors, Matrices, Probability and combinatorics, Series, Limits and continuity',
     linkToCompleteCourse: '#',
+    image: 'assets/Roulette70.svg',
   },
   {
     title: "Newton's laws",
     content: "Content: Newton's first law, Newton's third law and free body-diagrams, Newton's second law, Applications of Newton's second law",
     linkToCompleteCourse: '#',
+    image: 'assets/IncessantBlabber-Stylized-atom.svg',
   },
 ];
 
@@ -50,8 +56,9 @@ const favoriteCourse = arrOfFavoriteCourses.map((element) => {
   course.innerHTML = `
   <div class="background-card">
     <h2>${element.title}</h2>
-    <p class="display-while-hover">${element.content}</p>
     <a class="display-while-hover" href="${element.linkToCompleteCourse}">Start this course</a>
+    <p class="display-while-hover">${element.content}</p>
+    <img class="display-while-hover" src="${element.image}" alt="Toluene molecule">
   </div>
   `;
   return course;
